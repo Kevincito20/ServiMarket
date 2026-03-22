3b401e4304b264020b27a809be9c61995a0051b9
kevin <kevin.linares1@utp.ac.pa>
2026-03-21 19:35:18 -0500

    primer commit de servimarket

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('eslint/config');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+
+module.exports = defineConfig([
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+]);
